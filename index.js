e0d3e67f46991d9047b906e3eec138672b637efa
Purity Wambua <puritywamb@gmail.com>
2024-07-12 14:13:37 +0300

    dom content loaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+        // get element with id="text"
+        const paragraph = document.getElementById('text');
+    
+        // Replace the text content 
+        paragraph.textContent = 'This is really cool!';
+    });
